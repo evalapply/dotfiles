aba87cf8a63d4896f004dbdbb0083b3905f28d0d
Alex Afshar <contact@alexafshar.com>
2021-05-13 19:45:53 -0400

    Updated doom configs.

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,11 +1,11 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
-;; refresh' after modifying this file!
+;; sync' after modifying this file!
 
 
-;; These are used for a number of things, particularly for GPG configuration,
-;; some email clients, file templates and snippets.
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
 (setq user-full-name "Alex Afshar"
       user-mail-address "contact@alexafshar.com")
 
@@ -14,76 +14,90 @@
 ;;
 ;; + `doom-font'
 ;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
 (setq doom-font (font-spec :family "Fira Code" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. These are the defaults.
-(setq doom-theme 'doom-one)
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-vibrant)
 
-;; If you intend to use org, it is recommended you change this!
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-;; If you want to change the style of line numbers, change this to `relative' or
-;; `nil' to disable it:
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
+;; - `use-package!' for configuring packages
 ;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', where Emacs
-;;   looks when you load packages with `require' or `use-package'.
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
 ;; - `map!' for binding new keys
 ;;
 ;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
 ;;
-;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-;; Setup Scratch buffer for emacs-lisp experimentation.
-(setq initial-major-mode 'emacs-lisp-mode)
-(setq initial-scratch-message "\
-;; This is your Scratch buffer.
-;; Write some Elisp here:
-")
+;; Split windows right and bottom
+(setq evil-vsplit-window-right t
+      evil-split-window-below t)
 
-;; Add some aliases to eshell
-;; TODO: I think that these are now included in doom. Should check the repo.
-(add-hook 'eshell-mode-hook (lambda ()
-                              (map! "C-l" "clear")
-                              (eshell/alias "e" "find-file $1")
-                              (eshell/alias "d" "dired $1")
-                              (eshell/alias "g" 'magit-status)))
+;; Prompt for buffer on window split
+(defadvice! prompt-for-buffer (&rest _)
+  :after '(evil-window-split evil-window-vsplit)
+  (+ivy/switch-buffer))
 
-;; Add some extra filetypes to web-mode.
-(after! web-mode
-  (add-to-list 'auto-mode-alist '("\\.svelte?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.eex\\'" . web-mode)))
+;; Show buffer preview
+(setq +ivy-buffer-preview t)
 
-(add-hook 'before-save-hook 'gofmt-before-save)
+;; Default mode elisp
+(setq-default major-mode 'emacs-lisp-mode)
 
-;; Use EWW by default, this can be overriden with:
-;; & (eww-browse-with-external-browser) when entering url.
-(setq browse-url-browser-function 'eww-browse-url)
+(use-package! paredit
+              :hook (clojure-mode . paredit-mode))
 
-;; Map emmet expand to something other than tab, or it becomes annoying.
-(after! emmet-mode
-  (define-key emmet-mode-keymap (kbd "C-,") 'emmet-expand-line))
+;; (setq-hook! clojure-mode paredit-mode)
 
-;; Common LISP Hyperspec local documentation, instead of browser.
-(load! "/Users/evalapply/quicklisp/clhs-use-local.el" t)
+;; Clojure mode opens in emacs modee
+(add-to-list 'evil-emacs-state-modes 'clojure-mode)
 
-(after! eshell-git-prompt
-  (eshell-git-prompt-use-theme 'robbyrussell))
+(after! rustic
+  setq lsp-rust-server 'rust-analyzer)
 
-(add-hook! sly-mode
-           #'lispyville-mode)
+;; LiveView Sigil L syntax highlighting
+(require 'mmm-mode)
+(require 'web-mode)
+(setq mmm-global-mode 'maybe)
+(setq mmm-parse-when-idle 't)
+(setq mmm-set-file-name-for-modes '(web-mode))
+(custom-set-faces '(mmm-default-submode-face ((t (:background nil)))))
+(let ((class 'elixir-eex)
+    (submode 'web-mode)
+    (front "^[ ]+~L\"\"\"")
+    (back "^[ ]+\"\"\""))
+  (mmm-add-classes (list (list class :submode submode :front front :back back)))
+  (mmm-add-mode-ext-class 'elixir-mode nil class))
+
+(define-advice web-mode-guess-engine-and-content-type (:around (f &rest r) guess-engine-by-extension)
+  (if (and buffer-file-name (equal "ex" (file-name-extension buffer-file-name)))
+      (progn (setq web-mode-content-type "html")
+         (setq web-mode-engine "elixir")
+         (web-mode-on-engine-setted))
+    (apply f r)))
